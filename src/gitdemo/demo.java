26b860ee2f2f0ebb59712ba825a2fcdff040b152
wagh_sandeep <nandalalpawar04@gmail.com>
2023-02-02 11:55:17 +0530

    changes by Nandu

---
+++ src/gitdemo/demo.java
@@ -0,0 +1,25 @@
+package gitdemo;
+
+import java.util.Scanner;
+
+public class demo {
+
+	
+		
+	
+	public void addition() { 
+	int a=10;
+	int b=30;
+		int result=a+b;
+		System.out.println(" enter number");
+		Scanner sc =new Scanner(System.in);
+		
+//	a=sc.nextInt();
+//	b=sc.nextInt();
+	System.out.println(result);
+	
+	}
+	
+	
+}
+
---
+++ src/gitdemo/test.java
@@ -0,0 +1,8 @@
+package gitdemo;
+
+public class test {
+public static void main(String[] args) {
+	demo ad=new demo();
+	ad.addition();
+}
+}
